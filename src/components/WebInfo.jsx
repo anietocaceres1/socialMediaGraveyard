f73fc4caeda896a871a4ef0cadb4eecd70b3ff64
Andrés Nieto <anietocaceres1@gmail.com>
2025-05-08 19:37:14 +0200

    Modified ReadMe and AsSeenOn.jsx

--- src/components/WebInfo.jsx
+++ src/components/WebInfo.jsx
@@ -3,10 +3,19 @@ import "./WebInfo.css"
 function WebInfo(){
 
     return(
+        <>
         <div className="webInfo">
-            <p>The Social Media Graveyard is an open source list of all depracared Social Media Website. Our goal is to be a factual archive of all the social websites that cannot be accessed anymore</p>
-
+            <p>The Social Media Graveyard is an open source list of all depracared Social Media Website. Our goal is to be a factual archive of all the 
+                social websites that cannot be accessed anymore. <br></br>
+                Is there a social network missing in this page? Can you provide with more information? You can acces the Git repository here:</p>
+                <a href="https://github.com/anietocaceres1/socialMediaGraveyard">
+                    <img src="https://images.seeklogo.com/logo-png/30/2/github-logo-png_seeklogo-304612.png"/>
+                </a>
+                
         </div>
+        </>
+        
+
     )
 
 }
