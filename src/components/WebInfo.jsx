8447cbd0b8d67f8af4e6c26a7b55fea9d5c4e016
Andrés Nieto <anietocaceres1@gmail.com>
2025-05-09 15:18:17 +0200

    added link to another pestaña

--- src/components/WebInfo.jsx
+++ src/components/WebInfo.jsx
@@ -8,7 +8,7 @@ function WebInfo(){
             <p>The Social Media Graveyard is an open source list of all depracared Social Media Website. Our goal is to be a factual archive of all the 
                 social websites that cannot be accessed anymore. <br></br>
                 Is there a social network missing in this page? Can you provide with more information? You can acces the Git repository here:</p>
-                <a href="https://github.com/anietocaceres1/socialMediaGraveyard">
+                <a href="https://github.com/anietocaceres1/socialMediaGraveyard" target="_blank" rel="noopener noreferrer">
                     <img src="https://images.seeklogo.com/logo-png/30/2/github-logo-png_seeklogo-304612.png"/>
                 </a>
                 
